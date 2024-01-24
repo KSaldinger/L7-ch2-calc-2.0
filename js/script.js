d0f183b6d54d9963a16b5806f2980b3ebccc8e8b
katy_s <ksaldinger@gmail.com>
2024-01-23 21:41:11 -0800

    Updated js file

--- js/script.js
+++ js/script.js
@@ -1,10 +1,29 @@
 const subTypeElement = document.querySelector("#subscription");
 const subDurationElement = document.querySelector("#months");
 const result = document.querySelector(".result");
-const subType = "basic";
-const subDuration = 1;
+var subType = "basic";
+var subDuration = 1;
 
 subTypeElement.addEventListener("change", function (e) {
   subType = e.target.value;
-  console.log();
+  //console.log(subType);
+  updateSubscriptionDiv();
 });
+
+subDurationElement.addEventListener("change", function (e){
+  subDuration = Number(e.target.value);
+  //console.log(subDuration);
+  updateSubscriptionDiv();
+});
+
+const updateSubscriptionDiv = function () {
+  var monthlyCost = 5;
+  if (subType === "standard") {
+    monthlyCost = 7;
+  } else if (subType === "premium") {
+    monthlyCost = 10;
+  }
+  const total = subDuration * monthlyCost;
+  result.innerText = `You have chosen a ${subDuration} month ${subType} for $${total}.`;
+};
+
